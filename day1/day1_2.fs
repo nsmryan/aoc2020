b0ff9af4d4db0317f21a7cdf8ed96b86e471c8c1
Noah Ryan <nsmryan@gmail.com>
2020-12-01 10:52:06 -0500

    Minor changes to forth for day 1

--- day1/day1_2.fs
+++ day1/day1_2.fs
@@ -20,7 +20,8 @@ create expenses 201 cells allot
 : open s" day1.txt" r/o open-file throw file ! ;
 : line buffer max-line file @ read-line throw swap line-len ! ;
 : place num-expenses @ index ! num-expenses +1 ;
-: read begin line while buffer line-len @ s>number? 0 <> if drop place then repeat ;
+: process buffer line-len @ s>number? 0 <> if drop place then ;
+: read begin line while process repeat ;
 
 
 : check index @ rot index @ rot index @ + + 2020 = ;
@@ -30,9 +31,9 @@ create expenses 201 cells allot
        i j k check if i index0 ! j index1 ! k index2 ! then
        loop loop loop ;
 
-( : answer index rot index rot index * * ; )
 : answer index0 @ index @  index1 @ index @  index2 @ index @  * * ;
 : calculate find answer ;
 
-: solve open read calculate . ;
+: print cr . ;
+: solve open read calculate  print ;
 
